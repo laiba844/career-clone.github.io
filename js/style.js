b7d02a5fe9db3d8f80267284148d3bee6c500420
laiba844 <sheikhnilaiba@gmail.com>
2024-01-07 00:00:10 +0500

    career-clone

---
+++ js/style.js
@@ -0,0 +1,77 @@
+function checkbox(){
+    let fb=document.getElementById('fb').value;
+    let fb_control=document.getElementById('fb_control');
+    console.log(fb);
+    if(fb == "fb"){
+        fb_control.style.display='block'
+    }
+    else{
+        fb_control.style.display='none'
+    }
+    
+}
+function checkbox1(){
+    
+    let insta=document.getElementById('insta').value;
+    let insta_control=document.getElementById('insta_control');
+    console.log(insta);
+    
+    if(insta == "insta"){
+        insta_control.style.display='block'
+    }
+    else{
+        insta_control.style.display='none'
+    }
+}
+function checkbox2(){
+    
+    let ut=document.getElementById('ut').value;
+    let ut_control=document.getElementById('ut_control');
+    console.log(ut);
+    
+    if(ut == "ut"){
+        ut_control.style.display='block'
+    }
+    else{
+        ut_control.style.display='none'
+    }
+}
+function checkbox3(){
+    
+    let linkedin=document.getElementById('linkedin').value;
+    let linkedin_control=document.getElementById('linkedin_control');
+    console.log(linkedin);
+    
+    if(linkedin == "linkedin"){
+        linkedin_control.style.display='block'
+    }
+    else{
+        linkedin_control.style.display='none'
+    }
+}
+function checkbox4(){
+    
+    let Twitter=document.getElementById('Twitter').value;
+    let Twitter_control=document.getElementById('Twitter_control');
+    console.log(Twitter);
+    
+    if(Twitter == "Twitter"){
+        Twitter_control.style.display='block'
+    }
+    else{
+        Twitter_control.style.display='none'
+    }
+}
+function checkbox5(){
+    
+    let Other=document.getElementById('Other').value;
+    let Other_control=document.getElementById('Other_control');
+    console.log(Other);
+    
+    if(Other == "Other"){
+        Other_control.style.display='block'
+    }
+    else{
+        Other_control.style.display='none'
+    }
+}
